62ac331eada042d6d5f4688074e45e622146509e
LONG <doituyentoanlequydon@gmail.com>
2024-04-08 15:09:00 +0700

    first push

---
+++ main.py
@@ -0,0 +1,52 @@
+from knapsack.utils import read_testcase, write_testcase_csv, write_testcase_txt, get_filepath
+from ortools.algorithms.python import knapsack_solver
+import random
+import time
+
+def run_solver(values: list, weights: list, capacity: int, time_limit = 180):
+    
+    # Create the solver.
+    solver = knapsack_solver.KnapsackSolver(
+        knapsack_solver.SolverType.KNAPSACK_MULTIDIMENSION_BRANCH_AND_BOUND_SOLVER,
+        "KnapsackExample",
+    )
+
+    solver.init(values, [weights], [capacity])
+    solver.set_time_limit(time_limit)
+    start_time = time.time()
+    computed_value = solver.solve()
+    end_time = time.time()
+    packed_items = []
+    packed_weights = []
+    total_weight = 0
+    is_optimal = (end_time - start_time) <= time_limit - 1
+    for i in range(len(values)):
+        if solver.best_solution_contains(i):
+            packed_items.append(i)
+            packed_weights.append(weights[i])
+            total_weight += weights[i]
+    '''
+    print("Computed value:", computed_value)
+    print("Total weight:", total_weight)
+    print("Packed items:", packed_items)
+    print("Packed_weights:", packed_weights)
+    '''
+    return computed_value, total_weight, packed_items, packed_weights, is_optimal
+
+def main():
+    testcase_path_lst = get_filepath()
+    excecute_testcase_lst = [] 
+    for group in testcase_path_lst:
+        random_testcase_lst = random.sample(group, 5)
+        excecute_testcase_lst += random_testcase_lst
+
+    for id, testcase_path in enumerate(excecute_testcase_lst):
+        number_of_items, capacity, values, weights = read_testcase(testcase_path)
+        computed_value, total_weight, packed_items, packed_weights, is_optimal = run_solver(values, weights, capacity)
+        write_testcase_csv(testcase_path, computed_value, total_weight, is_optimal, id)
+        write_testcase_txt(testcase_path, computed_value, total_weight, packed_items, packed_weights, id)
+        print(f'Filepath: {testcase_path}')
+        print(f'Computed Value: {computed_value}')
+        print(f'Total Weight: {total_weight}')
+        print(f'Is Optimal: {is_optimal}')
+        print('--------------------------------------')
---
+++ utils.py
@@ -0,0 +1,75 @@
+import os
+import csv
+
+def read_testcase(filepath): 
+    '''
+    Output:
+    number_of_items: int
+    capacity: int
+    values: list
+    weights: list
+    '''
+    with open(filepath, 'r') as f:
+        raw_data = []
+        for line in f:
+            if line !='\n':
+                raw_data.append(line.strip())
+                
+        number_of_items = int(raw_data[0])
+        capacity = int(raw_data[1])
+        values = []
+        weights = []
+        for index_item in range(2, len(raw_data)):
+            value, weight = [int(num) for num in raw_data[index_item].split()]
+            values.append(value)
+            weights.append(weight)
+    return number_of_items, capacity, values, weights
+
+def write_testcase_csv(filepath, computed_value, total_weight, is_optimal, id = 0):
+    csv_path = os.path.join(os.getcwd(), 'knapsack_result.csv')
+    with open(csv_path, 'a', newline = '') as csvfile:
+        fieldnames = ['Filepath', 'Total Value', 'Total Weight', 'Is Optimal']
+        csvwriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        if id == 0:
+            csvwriter.writeheader()
+        csvwriter.writerow({
+            'Filepath': filepath,
+            'Computed Value': computed_value,
+            'Total Weight': total_weight,
+            'Is Optimal': is_optimal
+        })
+
+def write_testcase_txt(filepath, computed_value, total_weight, packed_items, packed_weights, id = 0):
+    txt_path = os.path.join(os.getcwd(), 'knapsack_result.txt')
+    with open(txt_path, 'a') as txtfile:
+        txtfile.write(f'------------------{id}------------------\n')
+        txtfile.write(f'Filepath: {filepath}\n')
+        txtfile.write(f'Computed Value: {computed_value}\n')
+        txtfile.write(f'Total Weight: {total_weight}\n')
+        txtfile.write(f'Packed Items: {packed_items}\n')
+        txtfile.write(f'Packed Weights: {packed_weights}\n')
+        txtfile.write('--------------------------------------\n')
+
+def get_filepath():
+    testcase_dir = os.path.join(os.getcwd(), 'kplib')
+    parent_dir_lst = [os.path.join(testcase_dir, dir) for dir in os.listdir(testcase_dir) if os.path.isdir(os.path.join(testcase_dir, dir))]
+
+    n_dir_lst = []
+    for parent_dir in parent_dir_lst:
+        if parent_dir != os.path.join(testcase_dir, '.git'):
+            n_dir_lst.append([os.path.join(parent_dir, n_dir) for n_dir in os.listdir(parent_dir)])
+
+    r_dir_lst = []
+    for lst_of_n_dir in n_dir_lst:
+        lst = []
+        lst += [os.path.join(n_dir, 'R01000') for n_dir in lst_of_n_dir]
+        lst += [os.path.join(n_dir, 'R10000') for n_dir in lst_of_n_dir]
+        r_dir_lst.append(lst)
+        
+    testcase_path_lst = []
+    for lst_of_r_dir in r_dir_lst:
+        lst = []
+        for r_dir in lst_of_r_dir:
+            lst += [os.path.join(r_dir, filename) for filename in os.listdir(r_dir)]
+        testcase_path_lst.append(lst)
+    return testcase_path_lst
